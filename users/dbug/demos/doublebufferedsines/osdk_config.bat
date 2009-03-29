0a133be8ad370ea048eaf02c082243eb8a981407
dbug <dbug@5b018df4-7644-4ca1-8539-644edd896026>
2009-03-29 11:19:38 +0000

    Added the 'double buffered sinus' demo

---
+++ users/dbug/demos/doublebufferedsines/osdk_config.bat
@@ -0,0 +1,9 @@
+@ECHO OFF
+
+::
+:: Set the build paremeters
+::
+SET OSDKADDR=$600
+SET OSDKNAME=SINES
+SET OSDKFILE=main picture vbl costable tables sprite 
+
---
+++ users/dbug/demos/doublebufferedsines/osdk_execute.bat
@@ -0,0 +1,51 @@
+@ECHO OFF
+
+
+::
+:: Initial check.
+:: Verify if the SDK is correctly configurated,
+::
+IF "%OSDK%"=="" GOTO ErCfg
+
+::
+:: Set the build paremeters
+::
+CALL osdk_config.bat
+
+::
+:: Check if the program was compiled
+::
+IF NOT EXIST build\%OSDKNAME%.TAP GOTO ErBld
+
+::
+:: Copy the compiled program into Euphoric folder 
+::
+COPY build\%OSDKNAME%.TAP %OSDK%\Euphoric\%OSDKNAME%.TAP
+
+::
+:: Execute the emulator
+::
+SET ORIC=%OSDK%\Euphoric\
+CALL %OSDK%\Euphoric\Euphoric.exe %OSDK%\Euphoric\%OSDKNAME%.TAP
+GOTO End
+
+
+::
+:: Outputs an error message about configuration
+::
+:ErCfg
+ECHO == ERROR ==
+ECHO The Oric SDK was not configured properly
+ECHO You should have a OSDK environment variable setted to the location of the SDK
+GOTO End
+
+::
+:: Outputs an error message about compilation
+::
+:ErBld
+ECHO == ERROR ==
+ECHO Before executing this program, you need to build it.
+ECHO Please run OSDK_BUILD.BAT before.
+GOTO End
+
+:End
