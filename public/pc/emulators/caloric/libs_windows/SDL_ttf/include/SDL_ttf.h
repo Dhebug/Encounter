2b422ab4a1f21aa846f981f3365083ca7686246e
Jede <Jede@5b018df4-7644-4ca1-8539-644edd896026>
2009-05-27 20:53:11 +0000

    Path changed

--- public/pc/emulators/caloric/libs_windows/SDL_ttf/include/SDL_ttf.h
+++ public/pc/emulators/caloric/libs_windows/SDL_ttf/include/SDL_ttf.h
@@ -30,8 +30,8 @@
 #ifndef _SDL_TTF_H
 #define _SDL_TTF_H
 
-#include "SDL.h"
-#include "begin_code.h"
+#include "SDL/SDL.h"
+#include "SDL/begin_code.h"
 
 /* Set up for C function definitions, even when using C++ */
 #ifdef __cplusplus
@@ -229,6 +229,6 @@ extern DECLSPEC int SDLCALL TTF_WasInit(void);
 #ifdef __cplusplus
 }
 #endif
-#include "close_code.h"
+#include "SDL/close_code.h"
 
 #endif /* _SDL_TTF_H */
