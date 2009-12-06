7d62cc8f2978693eefdd140ad23417b253f29e81
mmu_man <mmu_man@5b018df4-7644-4ca1-8539-644edd896026>
2009-12-06 00:36:50 +0000

    Add the Alchimie 2k9 invitro sources, hopefully all required to build.

---
+++ users/mmu_man/demo/INVAL2K9/generiqu.c
@@ -0,0 +1,22 @@
+/* -*- c-basic-offset: 8 -*-
+ * (c) 2008, Francois Revol <revol@free.fr>
+ *
+ */
+
+#include "global.h"
+
+const char *generique = \
+"\n" \
+"   VIP 2008\n" \
+"\n" \
+"     Code:\n" \
+" mmu_man" \
+"\n" \
+" LZ unpacker by" \
+" Dbug";
+
+
+void do_generique(void)
+{
+	
+}
---
+++ users/mmu_man/demo/INVAL2K9/musicasm.s
@@ -0,0 +1,4 @@
+
+
+_MusicIrq
+	rti
