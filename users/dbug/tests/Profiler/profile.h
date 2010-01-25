e6a78ad8412c0a71978d1e1ca0b6505c40035920
Mickaël Pointier <Dhebug@users.noreply.github.com>
2010-01-25 19:59:07 +0000

    Added a first rough version of the profiler - not optimized at all, still very much work in progress, does work only with C, please be patient.

---
+++ users/dbug/tests/Profiler/profile.h
@@ -0,0 +1,176 @@
+
+//
+// User settings
+//
+#define PROFILER_ENABLE		// Comment out to disable profiling
+
+//
+// List of routines (need an enum !)
+//
+#define ROUTINE_TEST20000			0
+#define ROUTINE_DOSOMETHING			1
+#define ROUTINE_DOSOMETHINGELSE		2
+#define ROUTINE_SUBROUTINE			3
+
+// Should be the number of routines to profile
+#define PROFILER_ROUTINE_COUNT   	4	
+
+#define PROFILER_MAX_CALLDEPTH		10
+
+//
+// Profiler API
+//
+#ifdef PROFILER_ENABLE
+// Profiler is enabled
+
+#ifdef PROFILER_ASM
+// Assembler API
+#else
+// C API
+extern unsigned char ProfilerRoutineCount[PROFILER_ROUTINE_COUNT];
+extern unsigned char ProfilerRoutineTimeLow[PROFILER_ROUTINE_COUNT];
+extern unsigned char ProfilerRoutineTimeHigh[PROFILER_ROUTINE_COUNT];
+extern unsigned int ProfilerFrameCount;
+extern unsigned char ProfilerFunctionId;
+
+void ProfilerInitialize();
+void ProfilerTerminate();
+void ProfilerNextFrame();
+void ProfilerDisplay();
+void ProfilerEnterFunction();
+void ProfilerLeaveFunction();
+
+#define PROFILE_ENTER(id) {ProfilerFunctionId=id;ProfilerEnterFunction();}
+#define PROFILE_LEAVE(id) {ProfilerFunctionId=id;ProfilerLeaveFunction();}
+#define PROFILE(id)
+
+#endif
+
+
+#else
+// Profiler is disabled, "do nothing" macros
+
+#define PROFILE_ENTER(id)
+#define PROFILE_LEAVE(id)
+#define PROFILE(id)
+
+#ifdef PROFILER_ASM
+// Assembler API
+#else
+// C API
+#define ProfilerInitialize()
+#define ProfilerTerminate()
+#define ProfilerNextFrame()
+#define ProfilerDisplay()
+#endif
+
+#endif
+
+/*
+
+I wonder if we could hack a profiler, which would compute the average 
+time/count spent in some routines, perhaps with a "enter/leave" function 
+that would take the value of the VIA timer, and increment a counter.
+
+Let's imagine a PROFILE macro, and a set of defines:
+
+#define PROFILER_ENABLE
+
+#define ROUTINE_PROJECTION 0
+#define ROUTINE_CLIPPING 1
+#define ROUTINE_DRAWING 2
+#define ROUTINE_AI 3
+#define ROUTINE_STARFIELD 4
+#define ROUTINE_COUNT 5
+
+etc...
+
+Then some tables to store the data:
+
+#ifdef PROFILER_ENABLE
+ProfilerRoutineCount      .dsb ROUTINE_COUNT
+ProfilerRoutineTimeLow .dsb ROUTINE_COUNT
+ProfilerRoutineTimeHigh .dsb ROUTINE_COUNT
+#endif
+
+Then the PROFILE macro itself, would be use that way:
+
+ProjectionRoutine
+    PROFILE_ENTER(ROUTINE_PROJECTION)
+    bla
+    bla
+    bla
+    jsr something
+    bne loopsomewhere
+    PROFILE_LEAVE(ROUTINE_PROJECTION)
+    rts
+
+This would increment the ProfilerRoutineCount entry for this routine, and 
+add the VIA timer to the ProfilerRoutineTimeLow/High.
+Obviously you have to put the LEAVE on every single exit point, which kind 
+of suck, so a solution for that would be to also have an additional smart 
+one that automaticaly does the LEAVE when exiting a routine.
+
+Something like that:
+
+ProjectionRoutine
+    PROFILE(ROUTINE_PROJECTION) // Do the ENTER and push the LEAVE
+    bla
+    bla
+    bla
+    jsr something
+    bne loopsomewhere
+    rts  // Automaticall calls the LEAVE function
+
+Of course all the macros should no nothing if the ENABLE_PROFILE is not 
+enabled.
+
+I guess the PROFILE routine could be doing something like that:
+
+    lda #<ProfileEnd
+    pha
+    lda #>ProfileEnd
+    pha
+
+So it would call the ProfileEnd when performing a rts in the profiled 
+function.
+Just an idea, need to think about it :)
+
+Perhaps something I could implement as displayed in the 3 last lines of the 
+screen :)
+
+Screen layout:
+
+0123456789012345678901234567890123456789
+Frame:0123
+ 1:0123/01 2:0123/01 3:0123/01 4:0123/01
+ 5:0123/01 6:0123/01 7:0123/01 8:0123/01
+
+0123456789012345678901234567890123456789
+Frame:0123
+ 1x01 2x01 3x01 4x01 5x01 6x01 7x01 8x01
+ 0123 0123 0123 0123 0123 0123 0123 0123
+ 
+
+0123456789012345678901234567890123456789
+Frame:0123 Cycles:012345 Fps:23
+ 0:012345 1:012345 2:012345 3:012345 
+ 4:012345 5:012345 6:012345 7:012345 
+ 
+012345 012345 012345 012345 012345 
+
+Infos for each profile group:
+- Number of calls
+- Total duration
+- Average duration
+- Percentage of frame
+
+
+
+ 
+
+ 
+ +0 +10 +20...
+*/
+
+
