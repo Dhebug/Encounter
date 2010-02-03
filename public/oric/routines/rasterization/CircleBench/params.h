5c021b4c5c567a469cc009f12845841e5a463a32
dbug <dbug@5b018df4-7644-4ca1-8539-644edd896026>
2010-02-03 20:00:30 +0000

    Modified the parameters to get a complete clipping window (more practical to find errors when testing), removed the useless variables.

--- public/oric/routines/rasterization/CircleBench/params.h
+++ public/oric/routines/rasterization/CircleBench/params.h
@@ -4,9 +4,10 @@
 #define _PARAMS_H_
 
 #define CLIP_LEFT	6
+//#define CLIP_LEFT	130
 #define CLIP_RIGHT	(239-6)
-#define CLIP_TOP	0 
-#define CLIP_BOTTOM	200
+#define CLIP_TOP	10 
+#define CLIP_BOTTOM	190
 
 
 #endif
