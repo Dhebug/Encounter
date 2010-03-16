b21b01cd73b9b36decc923df5251745c617545dd
Chema <Chema@5b018df4-7644-4ca1-8539-644edd896026>
2010-03-16 19:57:05 +0000

    New version of music. Changed a couple of details on mission code for testing (alpha). Now saved slot fires at mission 2.
    Removed a bug preventing a ship to become angry (testing a flag with ora, instead of and, silly me).

--- users/chema/TINE/oobj3d/overlay.s
+++ users/chema/TINE/oobj3d/overlay.s
@@ -61,7 +61,7 @@ __overlay_end
 		.byt 60                 ; Legal status 0=Clean, <50=Offender, >50=Fugitive
 		.byt 00					; Score, remainder
 		.word 10000             ; Current score
-		.byt 0                  ; Current mission
+		.byt 4                  ; Current mission
 		.word $0efe             ; Equipment flags
 		.byt 13			        ; Current player's ship
 		.byt 4					; Number of missiles
