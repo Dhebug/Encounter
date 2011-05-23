0dd3943984d60cdc2ea806cf322bc0cb074b8ff4
Chema <Chema@5b018df4-7644-4ca1-8539-644edd896026>
2011-05-23 18:50:36 +0000

    Old version... unused

--- users/chema/Elite Clone/Merge/txtcomp.bat
+++
@@ -1,5 +0,0 @@
-:: Generate grammar in disk sector... needs to generate a separate file
-:: tools\textcomp strings.s stringsc.s grammar.s
-
-:: Generate grammar in memory
-tools\textcomp dict.s dictc.s
