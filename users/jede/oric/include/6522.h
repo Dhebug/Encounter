ae30701f097c2796b301184c147407bf228efaa6
Jede <Jede@5b018df4-7644-4ca1-8539-644edd896026>
2011-08-20 23:19:38 +0000

    init

---
+++ users/jede/oric/include/6522.h
@@ -0,0 +1,19 @@
+#define	VIA_PORTB		$0300
+#define	VIA_PORTAH	$0301
+#define	VIA_DDRB		$0302
+#define	VIA_DDRA		$0303
+#define	VIA_T1CL		$0304
+#define	VIA_T1CH		$0305
+#define	VIA_T1LL		$0306
+#define	VIA_T1LH		$0307
+#define	VIA_T2LL		$0308
+#define	VIA_T2CH		$0309
+#define	VIA_SR		$030A
+#define	VIA_ACR		$030B
+#define	VIA_PCR		$030C
+#define	VIA_IFR		$030D
+#define	VIA_IER		$030E
+#define	VIA_PORTA		$030F
+
+
+#define	VIA2_PORTB	$0320
