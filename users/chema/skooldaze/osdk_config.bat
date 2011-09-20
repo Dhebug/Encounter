163259c6e179eda0ec4c486e8e4fa52f35247622
Chema <Chema@5b018df4-7644-4ca1-8539-644edd896026>
2011-09-20 18:25:51 +0000

    Song added. Still fat code, but works.

--- users/chema/skooldaze/osdk_config.bat
+++ users/chema/skooldaze/osdk_config.bat
@@ -8,6 +8,6 @@ SET OSDKCOMP=
 SET OSDKLINK=-B
 SET OSDKADDR=$500
 SET OSDKNAME=skooldaze
-SET OSDKFILE=init auxiliar graphics data grammar textc keyboard charset engine common script eric tail
+SET OSDKFILE=init auxiliar graphics data grammar textc keyboard charset engine common script eric sound tail
 ::SET OSDKFILE=init auxiliar graphics data text keyboard charset engine common script eric tail
 
