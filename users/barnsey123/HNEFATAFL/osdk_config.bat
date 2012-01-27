d53f49b9d406f31249b4a31a936f9bfc9c6bdbc9
barnsey123 <barnsey123@5b018df4-7644-4ca1-8539-644edd896026>
2012-01-27 13:34:21 +0000

    Using new title screen and Runic1 font

--- users/barnsey123/HNEFATAFL/osdk_config.bat
+++ users/barnsey123/HNEFATAFL/osdk_config.bat
@@ -5,5 +5,8 @@
 ::
 SET OSDKADDR=$600
 SET OSDKNAME=VIKING
-SET OSDKFILE=MAIN draw data pictiles explode border defence
+::SET OSDKFILE=MAIN draw data pictiles explode
+SET OSDKFILE=MAIN draw data pictiles explode runes font_6x8_runic1_full
+::SET OSDKFILE=MAIN draw data pictiles explode border runes
+::SET OSDKFILE=MAIN draw data pictiles explode border defence runes font_6x8_runic1_full
 
