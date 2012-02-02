f89e60043252f38f93b297439628bd67d0afe3a2
barnsey123 <barnsey123@5b018df4-7644-4ca1-8539-644edd896026>
2012-02-02 16:24:50 +0000

    memory reduced still further to 36924

--- users/barnsey123/HNEFATAFL/osdk_config.bat
+++ users/barnsey123/HNEFATAFL/osdk_config.bat
@@ -6,7 +6,8 @@
 SET OSDKADDR=$500
 SET OSDKNAME=VIKING
 ::SET OSDKFILE=MAIN draw data pictiles explode
-SET OSDKFILE=MAIN draw data pictiles explode runes text font_6x8_runic1_full
+::SET OSDKFILE=MAIN draw data pictiles explode runes text font_6x8_runic1_full
 ::SET OSDKFILE=MAIN draw data pictiles explode border runes
 ::SET OSDKFILE=MAIN draw data pictiles explode border defence runes font_6x8_runic1_full
+SET OSDKFILE=MAIN draw data pictiles explode runes text font_6x8_runic1_partial
 
