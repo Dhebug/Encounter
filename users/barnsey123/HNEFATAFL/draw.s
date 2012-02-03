06353649a2b1ed2c2a28685ed69a57371e4d57dc
Mickaël Pointier <Dhebug@users.noreply.github.com>
2012-02-03 22:31:46 +0000

    "inverse" and "drawcursor" are now written in assembler Size: 34155 bytes

--- users/barnsey123/HNEFATAFL/draw.s
+++ users/barnsey123/HNEFATAFL/draw.s
@@ -5,295 +5,81 @@
  .zero
  
  .text
-#define _rom_curset		$f0c8
-#define _rom_curmov		$f0fd
-#define _rom_draw		$f110
-#define _rom_fill		$f268
-#define _rom_circle		$f37f
-#define _params			$2e0
-#define	_rom_pattern	$213
 
-; draws the border
-/*
-_drawborder
+
+; Add 40 to "tmp1"
+_Add40
 .(
-	;curset(12,198,1)
-	lda #12
-	sta _params+1
-	lda #$c6
-	sta _params+3
-	lda #0
-	sta _params+2
-	sta _params+4
-	lda #1
-	sta _params+5	
-	jsr _rom_curset
-	
-	; draw(198,0,1)
-	lda #$c6
-	sta _params+1 
-	lda #0
-	sta _params+2
-	sta _params+3	
-	sta _params+4
-	lda #1
-	sta _params+5	
-	jsr _rom_draw
-	
-	; draw(0,-198,1)
-	lda #0 
-	sta _params+1 
-	sta _params+2
-	sec
-	sbc #$c6
-	sta _params+3
-	lda #$ff
-	sta _params+4
-	lda #1
-	sta _params+5	
-	jmp _rom_draw	
-.)
-*/
-;
-; draws a box
-/*
-_drawbox
-.(
-	; curset(tilex,tiley,3);
-	lda #12
-	sta _params+1 
-	lda #198
-	sta _params+3	
-	lda #0
-	sta _params+2
-	sta _params+4
-	lda #3
-	sta _params+5	
-	jsr _rom_curset
-	
-	; draw(size,0,fb);
-	lda #196 
-	sta _params+1 
-	lda #0
-	sta _params+2
-	sta _params+3	
-	sta _params+4
-	lda #1
-	sta _params+5	
-	jsr _rom_draw
-	
-	; draw(0,size,fb);
-	lda #0
-	sta _params+1 
-	sta _params+2
-	sta _params+4
-	lda #196 
-	sta _params+3	
-	lda #1
-	sta _params+5	
-	jsr _rom_draw
-
-	; draw(-size,0,fb);
-	lda #0
-	sta _params+3	
-	sta _params+4
-	sec
-	sbc #196 
-	sta _params+1 
-	lda #$ff
-	sta _params+2	
-	lda _fb
-	sta _params+5	
-	jsr _rom_draw
-
-	; draw(0,-size,fb);
-	lda #0
-	sta _params+1
-	sta _params+2	
-	sec
-	sbc #196 
-	sta _params+3	
-	lda #$ff
-	sta _params+4
-	lda #1
-	sta _params+5	
-	jmp _rom_draw
-.)
-*/ 
-
-/*
-; draw a diamond at x,y size z, foreground or background
-_drawdiamond
-.(
-	; curset(tilex,tiley,fb);
-	lda _tilex 
-	sta _params+1 
-	lda _tiley
-	sta _params+3	
-	lda #0
-	sta _params+2
-	sta _params+4
-	lda #3
-	sta _params+5	
-	jsr _rom_curset
-	
-	; draw(size,size,fb);
-	lda _size 
-	sta _params+1 
-	sta _params+3	
-	lda #0
-	sta _params+2
-	sta _params+4
-	lda _fb
-	sta _params+5	
-	jsr _rom_draw
-	
-	; draw(-size,size,fb);
-	lda #0
-	sta _params+4
-	sec
-	sbc _size 
-	sta _params+1 
-	lda #$ff
-	sta _params+2
-	lda _size 
-	sta _params+3	
-	lda _fb
-	sta _params+5	
-	jsr _rom_draw
-
-	; draw(-size,-size,fb);
-	lda #0
-	sec
-	sbc _size 
-	sta _params+1 
-	sta _params+3	
-	lda #$ff
-	sta _params+2	
-	sta _params+4
-	lda _fb
-	sta _params+5	
-	jsr _rom_draw
-
-	; draw(size,-size,fb);
-	lda _size 
-	sta _params+1
-	lda #0
-	sta _params+2	
-	sec
-	sbc _size 
-	sta _params+3	
-	lda #$ff
-	sta _params+4
-	lda _fb
-	sta _params+5	
-	jmp _rom_draw
-.)
-*/
-/*
-void drawcursor()
-{
-	//	- draw the cursor at cx,cy size z, foreground/background color (1 or 0)
-	char z=boxsize-2;
-	pattern(170);
-	curset(cx,cy,fb);
-	draw(z,0,fb);
-	draw(0,z,fb);
-	draw(-z,0,fb);
-	draw(0,-z,fb);
-	pattern(255);
-}
-*/
-
-_drawcursor
-.(
-	; char z=boxsize-2;
-	ldx _boxsize
-	dex
-	dex
-	stx tmp
-	
-	; pattern(170);
-	lda #170
-	sta _rom_pattern
-
-	; curset(cx,cy,fb);
-	lda _cx 
-	sta _params+1 
-	lda _cy
-	sta _params+3	
-	lda #0
-	sta _params+2
-	sta _params+4
-	lda _fb
-	sta _params+5	
-	jsr _rom_curset
-	
-	; draw(z,0,fb);
-	lda tmp 
-	sta _params+1 
-	lda #0
-	sta _params+2
-	sta _params+3	
-	sta _params+4
-	lda _fb
-	sta _params+5	
-	jsr _rom_draw
-	
-	; draw(0,z,fb);
-	lda #0
-	sta _params+1 
-	sta _params+2
-	sta _params+4	
-	lda tmp 
-	sta _params+3	
-	lda _fb
-	sta _params+5	
-	jsr _rom_draw
-	
-	; draw(-z,0,fb);
-	lda #0
-	sta _params+3	
-	sta _params+4	
-	sec
-	sbc tmp
-	sta _params+1 
-	lda #$ff
-	sta _params+2
-	lda _fb
-	sta _params+5	
-	jsr _rom_draw
-	
-	; draw(0,-z,fb);
-	lda #0
-	sta _params+1
-	sta _params+2	
-	sec
-	sbc tmp
-	sta _params+3 
-	lda #$ff
-	sta _params+4
-	lda _fb
-	sta _params+5	
-	jsr _rom_draw
-		
-	; pattern(255);
-	lda #255
-	sta _rom_pattern
+	clc
+	lda tmp1+0
+	adc #40
+	sta tmp1+0
+	bcc skip
+	inc tmp1+1
+skip	
 	rts
 .)
 
 
-/*
-_drawattackertile
+; X=number of columns
+; Out: update "tmp1"
+_AddCol
 .(
-	lda #13			; line length
-	sta _size
-	jmp _drawbox	; drawbox at x,y position
+	cpx #0
+loop
+	beq end
+	clc
+	lda tmp1+0
+	adc #3
+	sta tmp1+0
+	lda tmp1+1
+	adc #0
+	sta tmp1+1
+	dex
+	jmp loop
+end
+	rts
 .)
-*/
 
 
+; Y=number of rows
+; Out: update "tmp1"
+_AddRow
+.(
+	cpy #0
+loop
+	beq end
+	clc
+	lda tmp1+0
+	adc #<720
+	sta tmp1+0
+	lda tmp1+1
+	adc #>720
+	sta tmp1+1
+	dey
+	jmp loop
+end	
+	rts
+.)
+
+
+; x=column
+; y=row
+_SetScreenAddress
+.(
+	lda #<$a002
+	sta tmp1+0
+	lda #>$a002
+	sta tmp1+1
+		
+	; +col*3
+	jsr _AddCol
+	
+	; +row*720
+	jsr _AddRow
+	rts
+.)
+	
 
 /*
 void tileloop()
@@ -321,46 +107,10 @@ _tileloop
 	lda _ptr_graph+1
 	sta tmp0+1
 		
-	lda #<$a002
-	sta tmp1+0
-	lda #>$a002
-	sta tmp1+1
-	
-	; +col*3	
-	.(
 	ldx _col
-loop
-	beq end
-	clc
-	lda tmp1+0
-	adc #3
-	sta tmp1+0
-	lda tmp1+1
-	adc #0
-	sta tmp1+1
-	dex
-	jmp loop
-end
-	.)
-	
-	; +row*720
-	.(
-	ldx _row
-loop
-	beq end
-	clc
-	lda tmp1+0
-	adc #<720
-	sta tmp1+0
-	lda tmp1+1
-	adc #>720
-	sta tmp1+1
-	dex
-	jmp loop
-end	
-	.)
-	
-	
+	ldy _row
+	jsr _SetScreenAddress
+				
 	; Draw loop
 	.(
 	ldx #18
@@ -385,15 +135,7 @@ loop
 skip	
 	.)
 
-	.(
-	clc
-	lda tmp1+0
-	adc #40
-	sta tmp1+0
-	bcc skip
-	inc tmp1+1
-skip	
-	.)
+	jsr _Add40
 			
 	dex
 	bne loop
@@ -505,3 +247,155 @@ set_tile_to_draw
 	rts
 .)
 
+
+
+; Draw an inversed colr box to highlight selected box
+; _inversex=screen x position
+; _inversey=screen y position
+_inverse
+.(
+	ldx _inversex
+	ldy _inversey
+	jsr _SetScreenAddress
+	
+	jsr _Add40
+	jsr _Add40
+	
+	; Draw loop
+	.(
+	ldx #18-3
+loop
+	ldy #0
+	lda (tmp1),y
+	eor #%001111
+	sta (tmp1),y
+	iny
+	lda (tmp1),y
+	eor #%111111
+	sta (tmp1),y
+	iny
+	lda (tmp1),y
+	eor #%111110
+	sta (tmp1),y
+
+	jsr _Add40
+			
+	dex
+	bne loop
+	.)
+		
+	rts
+.)
+
+
+; _cx=x coordinate
+; _cy=y coordinate
+; _fb=0 -> Erase
+; _fb=1 -> Draw
+_drawcursor
+.(
+	ldx _cx
+	ldy _cy
+	jsr _SetScreenAddress
+	
+	jsr _Add40
+
+	lda _fb
+	beq erase
+		
+draw
+	.(
+	; top line
+	ldy #0
+	lda #%101010
+	sta (tmp1),y
+	iny
+	sta (tmp1),y
+	iny
+	sta (tmp1),y
+		
+		
+	; Draw loop
+	.(
+	ldx #8
+loop
+	jsr _Add40
+
+	; draw	
+	ldy #0
+	lda (tmp1),y
+	ora #%010000
+	sta (tmp1),y
+	iny
+	iny
+	lda (tmp1),y
+	ora #%000001
+	sta (tmp1),y
+	
+	jsr _Add40
+			
+	dex
+	bne loop
+	.)
+	
+	; bottom line
+	ldy #0
+	lda #%101010
+	sta (tmp1),y
+	iny
+	sta (tmp1),y
+	iny
+	sta (tmp1),y
+	
+	rts
+	.)
+
+erase
+	.(
+	; top line
+	ldy #0
+	lda #%1100000
+	sta (tmp1),y
+	iny
+	lda #%1000000
+	sta (tmp1),y
+	iny
+	sta (tmp1),y
+		
+		
+	; Draw loop
+	.(
+	ldx #8
+loop
+	jsr _Add40
+
+	; draw	
+	ldy #0
+	lda (tmp1),y
+	and #%11101111
+	sta (tmp1),y
+	iny
+	iny
+	lda (tmp1),y
+	and #%11111110
+	sta (tmp1),y
+	
+	jsr _Add40
+			
+	dex
+	bne loop
+	.)
+	
+	; bottom line
+	ldy #0
+	lda #%1100000
+	sta (tmp1),y
+	iny
+	lda #%1000000
+	sta (tmp1),y
+	iny
+	sta (tmp1),y
+	
+	rts
+	.)
+.)
