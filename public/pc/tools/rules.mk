b26106b7445706dc7883b525b9432e426dc87814
mmu_man <mmu_man@5b018df4-7644-4ca1-8539-644edd896026>
2012-09-25 22:56:17 +0000

    Add some defines
    * Add WIN32 for win32 builds
    * add [_N]DEBUG depending on $(RELEASE)

--- public/pc/tools/rules.mk
+++ public/pc/tools/rules.mk
@@ -2,6 +2,13 @@
 #HOSTOS := $(shell uname -s)
 #PLATFORM ?= $(HOSTOS)
 
+ifeq ($(RELEASE),)
+DEBUG = 1
+CPPFLAGS += -D_DEBUG
+else
+CPPFLAGS += -DNDEBUG
+endif
+
 ifeq ($(PLATFORM),win32)
 EXE = .exe
 .SUFFIXES: .exe
@@ -10,6 +17,7 @@ CC := $(CROSS_COMPILE)$(CC)
 CXX := $(CROSS_COMPILE)$(CXX)
 AR :=  $(CROSS_COMPILE)$(AR)
 WINDRES := $(CROSS_COMPILE)windres
+CPPFLAGS += -DWIN32
 
 # add default rules for exe files
 %.exe: %.o
