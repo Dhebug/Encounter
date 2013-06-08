698ed427aa8e2cb710ee88f04b2686cbcb13bd84
Mickaël Pointier <Dhebug@users.noreply.github.com>
2013-06-08 11:54:15 +0000

    Old 2002 prototype glued together from some existing code: This is not clean or fast, needs Turbo Mode to be at a decent speed.

---
+++ users/dbug/tests/Racing/osdk_execute.bat
@@ -0,0 +1,37 @@
+@ECHO OFF
+
+::SET OSDKDOSBOX=
+::SET OSDKEMUL=EUPHORIC
+
+::
+:: Initial check.
+:: Verify if the SDK is correctly configurated,
+::
+IF "%OSDK%"=="" GOTO ErCfg
+
+::
+:: Set the build paremeters
+::
+CALL osdk_config.bat
+
+
+::
+:: Run the emulator
+::
+CALL %OSDK%\Bin\execute.bat
+GOTO End
+
+
+::
+:: Outputs an error message about configuration
+::
+:ErCfg
+ECHO == ERROR ==
+ECHO The Oric SDK was not configured properly
+ECHO You should have a OSDK environment variable setted to the location of the SDK
+ECHO ===========
+IF "%OSDKBRIEF%"=="" PAUSE
+GOTO End
+
+:End
+::pause
