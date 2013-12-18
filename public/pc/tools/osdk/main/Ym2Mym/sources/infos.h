0963961f258f10d67abd4d4eb51a5624fcf1ad7c
dbug <dbug@5b018df4-7644-4ca1-8539-644edd896026>
2013-12-18 15:56:07 +0000

    Ym2Mym 1.0 - Added command line parameters for enabling/disabling retuning of frequency

---
+++ public/pc/tools/osdk/main/Ym2Mym/sources/infos.h
@@ -0,0 +1,21 @@
+
+
+#define TOOL_VERSION_MAJOR	1
+#define TOOL_VERSION_MINOR	0
+
+/*
+
+Change history for the Ym2Mym
+
+0.1 - 30.1.2000 
+- Marq/Lieves!Tuore & Fit (marq@iki.fi)
+
+0.2 - 3.2.2000  
+- Added a rude YM5 loader. Skips most of the header.
+
+1.0 - 2013/12/18
+- Added support for retuning (Atari ST songs are at 2mhz, Amstrad ones at 1mhz)
+- Added rude support for YM6 format (also skips most of the header)
+
+
+*/
