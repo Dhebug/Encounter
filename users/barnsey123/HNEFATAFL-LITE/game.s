27f8fa1ac38e5df82a4bed7eba301d9ced1676fa
barnsey123 <barnsey123@5b018df4-7644-4ca1-8539-644edd896026>
2014-01-25 15:42:25 +0000

    One part tape only version of HNEFATAFL WITHOUT skull mask/loader

---
+++ users/barnsey123/HNEFATAFL-LITE/game.s
@@ -0,0 +1,111 @@
+	
+_setfoundpiece10
+	lda #10
+	sta _foundpiece
+	rts
+	
+_setcheckmode1
+	lda #1
+	sta _checkroutemode
+	rts
+	
+_setcheckmode2
+	lda #2
+	sta _checkroutemode
+	rts
+	
+_setcheckmode3
+	lda #3
+	sta _checkroutemode
+	rts	
+
+_setcheckmode4
+	lda #4
+	sta _checkroutemode
+	rts
+		
+_incmodeone
+	inc _modeonevalid
+	rts
+	
+_incroute
+	inc _route
+	rts
+
+_decroute
+	dec _route
+	rts
+
+_incpoints
+	inc _points
+	rts
+
+_decpoints
+.(
+	dec _points
+	bne end
+	inc _points
+end
+	rts
+.)	
+	
+_setpoints
+	lda #10
+	sta _points
+	rts
+	
+_pointsplusten
+	clc
+	lda _points
+	adc #10
+	;adc _hightarget
+	sta _points
+	rts
+
+_incsurround
+	inc _surrounded
+	rts
+	
+_MultiplyPoints
+.(
+	lda _points
+	ldx _MultiPoints
+loop	
+	beq end
+	clc
+	adc #10
+	dex
+	bne loop
+end	
+	sta _points
+	rts
+.)	
+	
+_inccounter
+	inc _counter
+	rts
+
+_zeroarrow
+	lda #0
+	sta _arrow
+	rts
+	
+_zerocounter
+	lda #0
+	sta _counter
+	rts
+
+_zerofoundpiece
+	lda #0
+	sta _foundpiece
+	rts
+
+_inctakecounter
+	inc _takecounter
+	rts
+	
+;_getspace
+;	; get code of last key pressed
+;	lda $02df
+;	sta _mkey
+;	rts
