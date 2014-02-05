6b8fcdf07c095b52ead47d4a254432d7cd86c9da
barnsey123 <barnsey123@5b018df4-7644-4ca1-8539-644edd896026>
2014-02-05 17:17:30 +0000

    new vids and animation player

---
+++ users/barnsey123/LUNARCOM/osdk_makedata.bat
@@ -0,0 +1,12 @@
+@ECHO OFF
+
+
+:: For Main Program
+
+:: Video Call (BlahBlah)
+::%OSDK%\bin\pictconv VIDEO\blahblah.png BlahBlah.tap
+::%OSDK%\bin\pictconv -f0 -d1 -o4_BlahBlah VIDEO\blahblah.png BlahBlah.s
+%OSDK%\bin\pictconv -f0 -d1 -o4_LookRight VIDEO\look-right.png LookRight.s
+%OSDK%\bin\pictconv -f0 -d1 -o4_Tears VIDEO\tears.png Tears.s
+%OSDK%\bin\pictconv -f0 -d1 -o4_YouFuck VIDEO\youfuck.png YouFuck.s
+pause
