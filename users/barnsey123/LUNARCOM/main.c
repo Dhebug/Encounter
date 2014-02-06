55328cd7f98d0191ce090273fa724f637aa38b77
barnsey123 <barnsey123@5b018df4-7644-4ca1-8539-644edd896026>
2014-02-06 13:50:03 +0000

    more vids (need more efficient routines)

--- users/barnsey123/LUNARCOM/main.c
+++ users/barnsey123/LUNARCOM/main.c
@@ -14,6 +14,10 @@ SceneC: Agent Blue receives phone call from a shadowy figure known as the
 Rebel Commander. "We have him. Don't get in our way" 
 SceneD: Flashback: Pyong Yang: Rescue of Blue by Green from the Chinese Army of
 Occupation. 
+
+History
+04/02/2014 Started work with first few vids and sorted out display routines
+05/02/2014 added more vids
 */
 #include <lib.h>
 #define BLACK 0
@@ -27,8 +31,11 @@ Occupation.
 /* Definition of Global variables */
 extern unsigned char Tears[];		// video talking 
 extern unsigned char LookRight[];	// video looking right
+extern unsigned char Yabber[];		// video talking
 extern unsigned char YouFuck[];		// video "you fuck"
 extern unsigned char Whoops[];		// video whoops
+extern unsigned char NodYes[];		// video nodding
+extern unsigned char LeanForward[];	// video leaning forward
 unsigned char Frame;				// Frame of video to play
 int PauseTime,p;					// amount of time to Pause
 unsigned char* PtrGraphic;			// pointer to byte values of loaded picture
@@ -36,10 +43,16 @@ unsigned char InkColor;
 /* Listing of Functions */
 
 void Pause();		// adds pause to video playback
+void PlayLeanForward();	// LeanForward
+void PlayLeanForwardA();	
 void PlayLookRight();	// Look Right
 void PlayLookRightA();	
+void PlayNodYes();	// Nod yes
+void PlayNodYesA();
 void PlayTears();	// Pretend Tears
 void PlayTearsA();
+void PlayYabber();	// general talking
+void PlayYabberA();
 void PlayYouFuck();	// Go away you nasty man
 void PlayYouFuckA();
 void PlayWhoops();	// Did I swear?
@@ -71,19 +84,41 @@ void PlayVideo()	{
   // Look Right and back
   // look right
   PlayLookRight();
+  PlayYabber();PlayYabber();PlayYabber();
   PlayTears();
   PlayYouFuck();
   PlayWhoops();
+  PlayYabber();PlayYabber();
+  PlayNodYes();
+  PlayLeanForward();
   
-  PlayTears();
+  /*PlayTears();
   PlayYouFuck();
   PlayLookRight();
+  PlayLeanForward();
   PlayWhoops();
+  PlayNodYes();*/
   
 }
 void Pause(){
   for (p=0; p<PauseTime;p++){};
 }
+void PlayLeanForward(){
+	// look right
+	for (Frame=0;Frame<6;Frame++){
+	  PlayLeanForwardA();
+  	}
+  	// look back
+  	for (Frame=5;Frame>0;Frame--){
+	  PlayLeanForwardA();
+  	}
+}
+void PlayLeanForwardA(){
+	PtrGraphic=LeanForward;
+	DrawFrame();  
+	Pause();
+}
+
 void PlayLookRight(){
 	// look right
 	for (Frame=0;Frame<6;Frame++){
@@ -99,6 +134,23 @@ void PlayLookRightA(){
 	DrawFrame();  
 	Pause();
 }
+
+void PlayNodYes(){
+	// tears
+	for (Frame=0;Frame<6;Frame++){
+	  PlayNodYesA();
+  	}
+  	// tears back
+  	for (Frame=5;Frame>0;Frame--){
+	  PlayNodYesA();
+  	}
+}
+void PlayNodYesA(){
+	PtrGraphic=NodYes;
+	DrawFrame();  
+	Pause();
+}
+
 void PlayTears(){
 	// tears
 	for (Frame=0;Frame<6;Frame++){
@@ -111,6 +163,22 @@ void PlayTears(){
 }
 void PlayTearsA(){
 	PtrGraphic=Tears;
+	DrawFrame();  
+	Pause();
+}
+
+void PlayYabber(){
+	// tears
+	for (Frame=0;Frame<6;Frame++){
+	  PlayYabberA();
+  	}
+  	// tears back
+  	for (Frame=5;Frame>0;Frame--){
+	  PlayYabberA();
+  	}
+}
+void PlayYabberA(){
+	PtrGraphic=Yabber;
 	DrawFrame();  
 	Pause();
 }
