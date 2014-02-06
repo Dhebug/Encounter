4e17774f5e3a176bcd64512438ee85a812f7f9fc
barnsey123 <barnsey123@5b018df4-7644-4ca1-8539-644edd896026>
2014-02-06 13:50:03 +0000

    more vids (need more efficient routines)

--- users/barnsey123/LUNARCOM/osdk_makedata.bat
+++ users/barnsey123/LUNARCOM/osdk_makedata.bat
@@ -10,4 +10,7 @@
 %OSDK%\bin\pictconv -f0 -d1 -o4_Tears VIDEO\tears.png Tears.s
 %OSDK%\bin\pictconv -f0 -d1 -o4_YouFuck VIDEO\youfuck.png YouFuck.s
 %OSDK%\bin\pictconv -f0 -d1 -o4_Whoops VIDEO\whoops.png Whoops.s
+%OSDK%\bin\pictconv -f0 -d1 -o4_NodYes VIDEO\yes.png NodYes.s
+%OSDK%\bin\pictconv -f0 -d1 -o4_LeanForward VIDEO\leanforward.png LeanForward.s
+%OSDK%\bin\pictconv -f0 -d1 -o4_Yabber VIDEO\yabber.png Yabber.s
 pause
