e42f4a52522bd432a22a2e8cb848e7e736c7c631
barnsey123 <barnsey123@5b018df4-7644-4ca1-8539-644edd896026>
2014-02-11 19:02:38 +0000

    new playback routines, SHUTTLE, ASTEROID and ASTEROID2

---
+++ users/barnsey123/LUNARCOM/osdk_config_ASTEROID.bat
@@ -0,0 +1,11 @@
+@ECHO OFF
+
+::
+:: Set the build paremeters
+::
+SET OSDKADDR=$500
+SET OSDKNAME=ASTEROID
+SET OSDKTAPNAME=ASTEROID
+SET OSDKBIN=%OSDK%\bin
+
+SET OSDKFILE=ASTEROID drawASTEROID FlyOver00 FlyOver01 FlyOver02 FlyOver04 FlyOver05 FlyOver06 FlyOver07 FlyOver08 FlyOver09 FlyOver10 FlyOver11 FlyOver12 FlyOver13 FlyOver14 FlyOver15 FlyOver16 FlyOver17 FlyOver18 FlyOver19 FlyOver20 FlyOver21 FlyOver22 FlyOver23 FlyOver24 FlyOver25 FlyOver26 FlyOver27 FlyOver28 FlyOver29 FlyOver30 FlyOver31 FlyOver32 FlyOver33 FlyOver34 
