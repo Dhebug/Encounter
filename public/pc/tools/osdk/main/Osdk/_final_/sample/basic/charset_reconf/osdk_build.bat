84bc0fb82d4c4a183ceae148a378741b98c1c4d4
dbug <dbug@5b018df4-7644-4ca1-8539-644edd896026>
2014-03-16 15:52:34 +0000

    Updated Bas2Tap to fix a loading bug

--- public/pc/tools/osdk/main/Osdk/_final_/sample/basic/charset_reconf/osdk_build.bat
+++ public/pc/tools/osdk/main/Osdk/_final_/sample/basic/charset_reconf/osdk_build.bat
@@ -33,3 +33,4 @@ GOTO End
 
 
 :End
+pause
--- public/pc/tools/osdk/main/bas2tap/includes/infos.h
+++ public/pc/tools/osdk/main/bas2tap/includes/infos.h
@@ -1,7 +1,15 @@
+/*
 
+Change history for the Bas2Tap
 
+0.1 - First version, based on Fabrice Frances Txt2Bas
+
+0.2 - 2014/03/16
+- Fixed the missing byte that lead to Oricutron failing to load some basic files
+
+*/
 
 
 #define TOOL_VERSION_MAJOR	0
-#define TOOL_VERSION_MINOR	1
+#define TOOL_VERSION_MINOR	2
 
