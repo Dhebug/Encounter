9c7172a3a03b73fc6764b6fbf164cf10cb7a1a0b
dbug <dbug@5b018df4-7644-4ca1-8539-644edd896026>
2014-04-05 17:54:35 +0000

    Tested Microchess

---
+++ users/dbug/tests/WascolLinkerBug/osdk_build.bat
@@ -0,0 +1,37 @@
+@ECHO OFF
+CALL osdk_makedata.bat
+
+::
+:: Initial check.
+:: Verify if the SDK is correctly configurated
+::
+IF "%OSDK%"=="" GOTO ErCfg
+
+
+::
+:: Set the build paremeters
+::
+CALL osdk_config.bat
+
+
+::
+:: Launch the compilation of files
+::
+CALL %OSDK%\bin\make.bat %OSDKFILE%
+GOTO End
+
+
+::
+:: Outputs an error message
+::
+:ErCfg
+ECHO == ERROR ==
+ECHO The Oric SDK was not configured properly
+ECHO You should have a OSDK environment variable setted to the location of the SDK
+IF "%OSDKBRIEF%"=="" PAUSE
+GOTO End
+
+
+:End
+pause
+CALL osdk_execute.bat
---
+++ users/dbug/tests/WascolLinkerBug/osdk_config.bat
@@ -0,0 +1,8 @@
+@ECHO OFF
+
+::
+:: Set the build paremeters
+::
+SET OSDKADDR=$600
+SET OSDKNAME=chess
+SET OSDKFILE=main
