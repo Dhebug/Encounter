e34a9e75f495c7f8797c0cce65805c22d8caad29
mmu_man <mmu_man@5b018df4-7644-4ca1-8539-644edd896026>
2015-05-10 00:02:53 +0000

    old2mfm: sync with the other copy
    Is there any reason there are 2 version of this around anyway???

--- public/pc/tools/oric/old2mfm/old2mfm.c
+++ public/pc/tools/oric/old2mfm/old2mfm.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
+#include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
 FILE *fd;
-char bigbuf[1024*1024];
+char *bigbuf;
 char header[256];
 char trackbuf[6400];
-int sides, tracks, sectors, geometry=1;
+int32_t sides, tracks, sectors, geometry=1;
 int gap1,gap2,gap3;
 char old_signature[]="ORICDISK";
 char new_signature[]="MFM_DISK";
 
 void init_track(int n);
 void compute_crc(unsigned char *ptr,int count);
+
+/* TODO:Fix endianness! */
 
 int main(int argc,char *argv[])
 {
@@ -21,22 +24,30 @@ int main(int argc,char *argv[])
 	if (argc!=2) {
 		printf("Usage: oric2mfm <diskimage>\n");
 		printf("(converts old Euphoric disk image format to new MFM format)\n");
-		exit(1);
+		exit(EXIT_FAILURE);
 	}
 	fd=fopen(argv[1],"r+b");
 	if (fd==NULL) {
 		printf("Unable to open %s for read/write operation\n",argv[1]);
-		exit(1);
+		exit(EXIT_FAILURE);
 	}
 	fread(header,8,1,fd);
 	if (strncmp(header,old_signature,8)!=0) {
 		printf("%s is not an old disk image\n",argv[1]);
-		exit(1);
+		exit(EXIT_FAILURE);
 	}
-	fread(&sides,1,4,fd); fread(&tracks,1,4,fd); fread(&sectors,1,4,fd);
+	fread(&sides,1,sizeof(int32_t),fd);
+	fread(&tracks,1,sizeof(int32_t),fd);
+	fread(&sectors,1,sizeof(int32_t),fd);
 	fread(header+20,256-20,1,fd);
+	bigbuf = malloc(sides*tracks*sectors*256);
+	if (bigbuf==NULL) {
+		printf("Unable to allocate memory\n");
+		exit(EXIT_FAILURE);
+	}
 	fread(bigbuf,sides*tracks*sectors,256,fd);
 	fseek(fd,0,SEEK_SET);
+printf("read old.\n");
 
         switch (sectors) {
                 case 15: case 16: case 17:
@@ -47,33 +58,39 @@ int main(int argc,char *argv[])
                         break;
                 default:
                         printf("unrealistic sectors per track number\n");
-                        exit(1);
+                        exit(EXIT_FAILURE);
         }
 	init_track(sectors);
 
+printf("writing header...\n");
 	fwrite(new_signature,8,1,fd);
-	fwrite(&sides,1,4,fd); fwrite(&tracks,1,4,fd); fwrite(&geometry,1,4,fd);
+	fwrite(&sides,1,sizeof(int32_t),fd);
+	fwrite(&tracks,1,sizeof(int32_t),fd);
+	fwrite(&geometry,1,sizeof(int32_t),fd);
 	fwrite(header+20,256-20,1,fd);
 
 	for(s=0;s<sides;s++)
 	  for(t=0;t<tracks;t++) {
             offset=gap1;
 	    for(i=0;i<sectors;i++) {
+printf("writing size %d track %d sector %d...\n", s, t, i);
               trackbuf[offset+4]=t;
               trackbuf[offset+5]=s;
               trackbuf[offset+6]=i+1;
               trackbuf[offset+7]=1;
-              compute_crc((unsigned char*)(trackbuf+offset),4+4);
+              compute_crc(trackbuf+offset,4+4);
               offset+=4+6;
               offset+=gap2;
               memcpy(trackbuf+offset+4,bigbuf+((s*tracks+t)*sectors+i)*256,256);
-              compute_crc((unsigned char*)(trackbuf+offset),4+256);
+              compute_crc(trackbuf+offset,4+256);
               offset+=256+6;
               offset+=gap3;
 	    }
 	    fwrite(trackbuf,6400,1,fd);
 	  }
-    return 0;
+printf("done.\n");
+	free(bigbuf);
+	return EXIT_SUCCESS;
 }
 
 void init_track(int n)
@@ -109,14 +126,14 @@ unsigned int crctab[256] =
 	0xB75B, 0xA77A, 0x9719, 0x8738, 0xF7DF, 0xE7FE, 0xD79D, 0xC7BC,
 	0x48C4, 0x58E5, 0x6886, 0x78A7, 0x0840, 0x1861, 0x2802, 0x3823,
 	0xC9CC, 0xD9ED, 0xE98E, 0xF9AF, 0x8948, 0x9969, 0xA90A, 0xB92B,
-	0x5AF5, 0x4AD4, 0x7AB7, 0x6A96, 0x1A71, 0x0A50, 0x3A33, 0x2A12,
-	0xDBFD, 0xCBDC, 0xFBBF, 0xEB9E, 0x9B79, 0x8B58, 0xBB3B, 0xAB1A,
-	0x6CA6, 0x7C87, 0x4CE4, 0x5CC5, 0x2C22, 0x3C03, 0x0C60, 0x1C41,
-	0xEDAE, 0xFD8F, 0xCDEC, 0xDDCD, 0xAD2A, 0xBD0B, 0x8D68, 0x9D49,
+	0x5AF5, 0x4AD4, 0x7AB7, 0x6A96, 0x1A71, 0x0A50, 0x3A33, 0x2A12, 
+	0xDBFD, 0xCBDC, 0xFBBF, 0xEB9E, 0x9B79, 0x8B58, 0xBB3B, 0xAB1A, 
+	0x6CA6, 0x7C87, 0x4CE4, 0x5CC5, 0x2C22, 0x3C03, 0x0C60, 0x1C41, 
+	0xEDAE, 0xFD8F, 0xCDEC, 0xDDCD, 0xAD2A, 0xBD0B, 0x8D68, 0x9D49, 
 	0x7E97, 0x6EB6, 0x5ED5, 0x4EF4, 0x3E13, 0x2E32, 0x1E51, 0x0E70,
-	0xFF9F, 0xEFBE, 0xDFDD, 0xCFFC, 0xBF1B, 0xAF3A, 0x9F59, 0x8F78,
+	0xFF9F, 0xEFBE, 0xDFDD, 0xCFFC, 0xBF1B, 0xAF3A, 0x9F59, 0x8F78, 
 	0x9188, 0x81A9, 0xB1CA, 0xA1EB, 0xD10C, 0xC12D, 0xF14E, 0xE16F,
-	0x1080, 0x00A1, 0x30C2, 0x20E3, 0x5004, 0x4025, 0x7046, 0x6067,
+	0x1080, 0x00A1, 0x30C2, 0x20E3, 0x5004, 0x4025, 0x7046, 0x6067, 
 	0x83B9, 0x9398, 0xA3FB, 0xB3DA, 0xC33D, 0xD31C, 0xE37F, 0xF35E,
 	0x02B1, 0x1290, 0x22F3, 0x32D2, 0x4235, 0x5214, 0x6277, 0x7256,
 	0xB5EA, 0xA5CB, 0x95A8, 0x8589, 0xF56E, 0xE54F, 0xD52C, 0xC50D,
@@ -132,8 +149,8 @@ unsigned int crctab[256] =
 	0xEF1F, 0xFF3E, 0xCF5D, 0xDF7C, 0xAF9B, 0xBFBA, 0x8FD9, 0x9FF8,
 	0x6E17, 0x7E36, 0x4E55, 0x5E74, 0x2E93, 0x3EB2, 0x0ED1, 0x1EF0
 };
-
-void compute_crc(unsigned char *ptr, int count)
+	
+void compute_crc(unsigned char *ptr,int count)
 {
 	int i;
 	unsigned short crc=0xFFFF,byte;
