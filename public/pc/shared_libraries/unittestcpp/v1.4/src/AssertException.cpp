fdd42c40443d8df2d53bed3faa955de2d12c3038
Mickaël Pointier <Dhebug@users.noreply.github.com>
2015-10-24 17:16:43 +0000

    coFixed Coverity issues: - 22116 Non checked string copies - 22140 False positive, supposedly non initialized member

--- public/pc/shared_libraries/unittestcpp/v1.4/src/AssertException.cpp
+++ public/pc/shared_libraries/unittestcpp/v1.4/src/AssertException.cpp
@@ -1,34 +1,37 @@
 #include "AssertException.h"
 #include <cstring>
 
-namespace UnitTest {
+namespace UnitTest
+{
 
-AssertException::AssertException(char const* description, char const* filename, int lineNumber)
+  AssertException::AssertException(char const* description, char const* filename, int lineNumber)
     : m_lineNumber(lineNumber)
-{
-	using namespace std;
+  {
+    using namespace std;
 
-    strcpy(m_description, description);
-    strcpy(m_filename, filename);
-}
+    strncpy(m_description, description,sizeof(m_description));
+    m_description[sizeof(m_description)-1]=0;
+    strncpy(m_filename, filename,sizeof(m_filename));
+    m_filename[sizeof(m_filename)-1]=0;
+  }
 
-AssertException::~AssertException() throw()
-{
-}
+  AssertException::~AssertException() throw()
+  {
+  }
 
-char const* AssertException::what() const throw()
-{
+  char const* AssertException::what() const throw()
+  {
     return m_description;
-}
+  }
 
-char const* AssertException::Filename() const
-{
+  char const* AssertException::Filename() const
+  {
     return m_filename;
-}
+  }
 
-int AssertException::LineNumber() const
-{
+  int AssertException::LineNumber() const
+  {
     return m_lineNumber;
-}
+  }
 
 }
