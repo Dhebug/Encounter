039d3a8d9c0af87882d04e622171bff005985ea0
mmu_man <mmu_man@5b018df4-7644-4ca1-8539-644edd896026>
2016-06-07 20:20:51 +0000

    ttflist is actually a bash script.

--- users/mmu_man/ttf2oric/ttflist.sh
+++ users/mmu_man/ttf2oric/ttflist.sh
@@ -1,3 +1,3 @@
-#!/bin/sh
+#!/bin/bash
 
 echo > Fonts.h; for f in *_*.h; do echo "#include \"$f\"" >> Fonts.h; done; echo "" > FontList.h; for f in *_*.h; do echo "	{ \"$f\", ${f/.h/_bits} }," >> FontList.h; done
