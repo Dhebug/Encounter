7d46ae594423885839f17018a8835f8329548ec0
dbug <dbug@5b018df4-7644-4ca1-8539-644edd896026>
2018-01-27 14:32:10 +0000

    Just committing a cleaned up version of the previous game jam game code with just an added morse code table in Buffer.s

--- users/dbug/games/GlobalGameJam2018/code/floppy_description.h
+++ users/dbug/games/GlobalGameJam2018/code/floppy_description.h
@@ -7,10 +7,10 @@
 // Information for the Assembler
 //
 #ifdef LOADER
-FileStartSector .byt 134,134,137,140,135
-FileStartTrack .byt 0,1,1,1,2
-FileSizeLow .byt <9728,<760,<1536,<8000,<8000
-FileSizeHigh .byt >9728,>760,>1536,>8000,>8000
+FileStartSector .byt 134,142,145,131,145
+FileStartTrack .byt 0,0,0,1,1
+FileSizeLow .byt <3840,<760,<1536,<8000,<8000
+FileSizeHigh .byt >3840,>760,>1536,>8000,>8000
 #undef LOADER
 #endif // LOADER
 #undef ASSEMBLER
@@ -35,17 +35,17 @@ FileSizeHigh .byt >9728,>760,>1536,>8000,>8000
 // List of files written to the floppy
 //
 // Entry #0 '..\build\files\game.o'
-// - Starts on  track 0 sector 6 and is 17 sectors long (4236 compressed bytes: 43% of 9728 bytes).
+// - Starts on  track 0 sector 6 and is 8 sectors long (1832 compressed bytes: 47% of 3840 bytes).
 // Entry #1 '..\build\files\Font6x8.hir'
-// - Starts on  track 1 sector 6 and is 3 sectors long (556 compressed bytes: 73% of 760 bytes).
+// - Starts on  track 0 sector 14 and is 3 sectors long (556 compressed bytes: 73% of 760 bytes).
 // Entry #2 '..\build\files\Font6x6.hir'
-// - Starts on  track 1 sector 9 and is 3 sectors long (554 compressed bytes: 36% of 1536 bytes).
+// - Starts on  track 0 sector 17 and is 3 sectors long (554 compressed bytes: 36% of 1536 bytes).
 // Entry #3 '..\build\files\GlobalGameJamOslo.hir'
-// - Starts on  track 1 sector 12 and is 12 sectors long (2971 compressed bytes: 37% of 8000 bytes).
+// - Starts on  track 1 sector 3 and is 14 sectors long (3337 compressed bytes: 41% of 8000 bytes).
 // Entry #4 '..\build\files\title_screen.hir'
-// - Starts on  track 2 sector 7 and is 9 sectors long (2065 compressed bytes: 25% of 8000 bytes).
+// - Starts on  track 1 sector 17 and is 12 sectors long (2977 compressed bytes: 37% of 8000 bytes).
 //
-// 49 sectors used, out of 1428. (3% of the total disk size used)
+// 45 sectors used, out of 1428. (3% of the total disk size used)
 //
 #define LOADER_SECTOR_BUFFER $200
 #define LOADER_BASE_ZERO_PAGE $00
@@ -53,8 +53,8 @@ FileSizeHigh .byt >9728,>760,>1536,>8000,>8000
 #define LOADER_GAME_PROGRAM_ADDRESS $c000
 #define LOADER_GAME_PROGRAM_TRACK 0
 #define LOADER_GAME_PROGRAM_SECTOR 134
-#define LOADER_GAME_PROGRAM_SIZE 9728
-#define LOADER_GAME_PROGRAM_SIZE_COMPRESSED 4236
+#define LOADER_GAME_PROGRAM_SIZE 3840
+#define LOADER_GAME_PROGRAM_SIZE_COMPRESSED 1832
 #define LOADER_FONT_6x8 1
 #define LOADER_FONT_6x6 2
 #define LOADER_FONT_6x6_SIZE 1536
--- users/dbug/games/GlobalGameJam2018/code/main.c
+++ users/dbug/games/GlobalGameJam2018/code/main.c
@@ -9,6 +9,28 @@
 
 extern void System_InstallIRQ_SimpleVbl();
 extern unsigned char Font6x6[];
+extern void InitializeTables();
+
+char MyString[]="This is a test";
+
+void BigHackTestYeahhhh()
+{
+    poke(0xbfdf,26);    // Back to TEXT mode by poking on the last byte of the screen with the TEXT 50hz attribute
+
+    //memcpy((void*)(0xB400),(void*)(0x9800),1024);
+
+    memset(0xbb80,64,64);   // Whatever
+    poke(0xbb80,16+1);      // Red paper on the first byte
+
+	while (1)
+	{
+
+	}
+
+    memcpy((void*)(0xbb80+1),MyString,14);
+
+	poke(0xbb80+40,65);
+}
 
 
 void LoadTitleScreen()
@@ -22,10 +44,16 @@ void main()
 	// Install the IRQ handler
 	System_InstallIRQ_SimpleVbl();
 
+	InitializeTables();
+
 	// Load the 6x8 font
 	LoadFileAt(LOADER_FONT_6x8,0x9900);
 	LoadFileAt(LOADER_FONT_6x6,Font6x6);
 	PatchFont();
+
+
+    //BigHackTestYeahhhh();
+
 
     // Load the title picture
     LoadFileAt(LOADER_GAMEJAM_LOGO,0xa000); 
