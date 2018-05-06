cc07812e1c4cc915315693a6776be3519d182d3c
Mickaël Pointier <Dhebug@users.noreply.github.com>
2018-05-06 13:55:26 +0000

    Bas2Tap 1.1 - Added filtering of strings before parsing, to remove superfluous spaces, tabs and other carriage returns before starting the syntax conversion pass

--- public/pc/tools/osdk/main/bas2tap/includes/infos.h
+++ public/pc/tools/osdk/main/bas2tap/includes/infos.h
@@ -11,9 +11,12 @@ Change history for the Bas2Tap
 - Added the handling of token codes over 246 to allow the dump of corrupted files, these will have a CORRUPTED_ERROR_CODE_nnn message at the location of the corrupted data (in this particular case it was the game ENCOUNTER from Severn Software at line 11150 having a "NEXT WITHOUT FOR" message displayed
 - Fixed the program so it can output to a file instead of stdout
 
+1.1 - 2018/04/07
+- Added filtering of strings before parsing, to remove superfluous spaces, tabs and other carriage returns before starting the syntax conversion pass
+
 */
 
 
 #define TOOL_VERSION_MAJOR	1
-#define TOOL_VERSION_MINOR	0
+#define TOOL_VERSION_MINOR	1
 
