99d86be5caff47fb7134c87ffa904c982ee0e7db
dbug <dbug@5b018df4-7644-4ca1-8539-644edd896026>
2018-06-30 14:31:32 +0000

    The zeropage copy example is now minimalist, should be much easier to understand by everybody.

--- users/dbug/tests/ZeroPageCode/main.s
+++ users/dbug/tests/ZeroPageCode/main.s
@@ -2,26 +2,22 @@
 	.text
 
 _main
-	jmp _main
 	jsr CopyZeroPageRoutine	
-	jsr ZeroPageRoutine
-	jsr SwapBuffer
-	jsr SwapBuffer
-	rts
+	jmp ZeroPageRoutine
 	
 StartZeroPageRoutine 
 	*=$50
  
 ZeroPageRoutine 
+.(
 	nop
-source 
-	lda $A000 
-    sta $030F 
-	inc source+1 
-	bne skip1 
-	inc source+2    
-skip1  
-	rts
+loop	
+	ldx #16+1
+	stx $BB80
+	ldx #16+4
+	stx $BB80
+	jmp loop
+.)	
 EndZeroPageRoutine
 
 	*=StartZeroPageRoutine+EndZeroPageRoutine-ZeroPageRoutine
@@ -38,23 +34,3 @@ loop
 	rts
 .)
 
-
-SwapBuffer  .dsb 256
-
-SwapZeroPage
-	jmp SwapZeroPage
-.(	
-	ldy #0
-loop
-	lda $00,y
-	tax
-	lda SwapBuffer,y
-	sta $00,y
-	txa
-	sta SwapBuffer,y
-	iny
-	bne loop
-	rts
-.)
-
-		
