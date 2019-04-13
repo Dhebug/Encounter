d5878da8eec312027d4b9b51995a00db825b9c39
dbug <dbug@5b018df4-7644-4ca1-8539-644edd896026>
2019-04-13 12:00:38 +0000

    FilePack: Updated the version number after somebody reported an issue with a veryyyyyyy old version which happened to have the same version number of a much more recent one.

--- public/pc/tools/osdk/main/filepack/includes/infos.h
+++ public/pc/tools/osdk/main/filepack/includes/infos.h
@@ -1,7 +1,21 @@
+/*
+
+Change history for the FilePack
+
+0.1 to 0.3 - First version, based on MIT's packer
+
+0.4 - 2009/05/24
+- Added the -p1 option
+
+0.5 - 2010/01/23
+- Added the -mn and -p2 options
+
+
+*/
 
 
 
 
 #define TOOL_VERSION_MAJOR	0
-#define TOOL_VERSION_MINOR	2
+#define TOOL_VERSION_MINOR	5
 
