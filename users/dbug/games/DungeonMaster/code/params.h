5a90cdc2f3ff3607fec8de732775353845969afb
Mickaël Pointier <Dhebug@users.noreply.github.com>
2020-08-15 07:01:10 +0000

    Source Code for the Dungeon Master intro which was explained in the OSDK article https://osdk.org/index.php?page=articles&ref=ART15 as well as the YouTube video https://www.youtube.com/watch?v=holCw1F-QWg

---
+++ users/dbug/games/DungeonMaster/code/params.h
@@ -0,0 +1,23 @@
+
+/*This keys don't have an ASCII code assigned, so we will
+ use consecutive values outside the usual alphanumeric 
+ space.
+*/
+
+#define KEY_UP			1
+#define KEY_LEFT		2
+#define KEY_DOWN		3
+#define KEY_RIGHT		4
+#define KEY_LCTRL		5
+#define KET_RCTRL		6
+#define KEY_LSHIFT		7
+#define KEY_RSHIFT		8
+#define KEY_FUNCT		9
+#define KEY_ESC			10
+#define KEY_DEL			11
+#define KEY_RETURN		12
+
+
+
+
+
