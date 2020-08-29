1e402a83fa58fabba7ee89c0e29003aeced76181
dbug <dbug@5b018df4-7644-4ca1-8539-644edd896026>
2020-08-29 17:59:27 +0000

    Added my old (2001) raycast prototype

---
+++ users/dbug/tests/Raycast/osdk_config.bat
@@ -0,0 +1,16 @@
+@ECHO OFF
+
+::
+:: Set the build paremeters
+::
+SET OSDKADDR=$600
+SET OSDKNAME=WALLS
+SET OSDKFILE=main costable scroll gencode
+
+::  costable
+::pat_picture pat_font draw 
+
+
+
+
+
