8e23c5dc0b4f6d97bbc62aec0f5cdd9c07f4ed7e
Mickaël Pointier <Dhebug@users.noreply.github.com>
2020-09-05 10:48:27 +0000

    Added the support for < and > keys. See article on https://osdk.org/index.php?page=articles&ref=ART16

--- public/oric/routines/single_row_keyboard_read/defines.h
+++ public/oric/routines/single_row_keyboard_read/defines.h
@@ -5,12 +5,12 @@
 // If you want other values, then you need to modify the keyboard 
 // reading code.
 
-#define SPACE       1
-#define UNUSED_1    2
-#define UNUSED_2    4
-#define UP_ARROW    8
-#define LEFT_SHIFT  16
-#define LEFT_ARROW  32
-#define DOWN_ARROW  64
-#define RIGHT_ARROW 128
+#define KEY_SPACE        1
+#define KEY_LESS_THAN    2
+#define KEY_GREATER_THAN 4
+#define KEY_UP_ARROW     8
+#define KEY_LEFT_SHIFT   16
+#define KEY_LEFT_ARROW   32
+#define KEY_DOWN_ARROW   64
+#define KEY_RIGHT_ARROW 128
 
