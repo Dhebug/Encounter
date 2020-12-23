3790cfff9fbfbd8bd0802fcb972ba24b0b1728c0
Mickaël Pointier <Dhebug@users.noreply.github.com>
2020-12-23 18:29:07 +0000

    Link65 0.67 - Error messages now indicate that they came from the Linker, because a generic "Error, can't load xxxx" was not very informative

--- public/pc/tools/osdk/main/header/includes/infos.h
+++ public/pc/tools/osdk/main/header/includes/infos.h
@@ -5,8 +5,11 @@ Change history for Header
 0.2 - 2014/05/11
 - The address can now be specified with either $ or 0x as an hexadecimal prefix
 
+1.0 - 2020/12/12
+- Added the flag -b to specify if the converted file should expose 00 (BASIC) or 80 (Binary) in the header
+
 */
 
-#define TOOL_VERSION_MAJOR	0
-#define TOOL_VERSION_MINOR	2
+#define TOOL_VERSION_MAJOR	1
+#define TOOL_VERSION_MINOR	0
 
