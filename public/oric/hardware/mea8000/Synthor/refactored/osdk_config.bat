c1125ee654ca589685859de242a7c08b86866586
Mickaël Pointier <Dhebug@users.noreply.github.com>
2022-06-18 14:52:39 +0000

    Optimized version of the Synthor software. It's technically functionally identical, but instead of using DATA statements to generate the binary payload the binary data is directly loaded from disk. Doing so reduces the size of the program significantly, and reduces the startup time by almost two minutes (down to 5 seconds when loading from disk

---
+++ public/oric/hardware/mea8000/Synthor/refactored/osdk_config.bat
@@ -0,0 +1,15 @@
+@ECHO OFF
+
+::
+:: Set the build paremeters
+::
+SET OSDKNAME=Synthor2
+SET OSDKTAPNAME=Synthor2
+
+SET OSDKFILE=Synthor2
+::SET OSDKFILE=sor
+SET OSDKDISK=Synthor2
+
+SET OSDKINIST="CLS:SYNTHOR2"
+
+
