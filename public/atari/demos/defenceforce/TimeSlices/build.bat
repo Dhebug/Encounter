f64735a517da6acd7a36a617dfe58d1d2699b7cc
Mickaël Pointier <Dhebug@users.noreply.github.com>
2023-01-03 08:53:10 +0000

    TimeSlices v1.2 actually fixed the monochrome screen detection, was not actually properly working... (facepalm) Also cleaned-up the build script to be less messy.

--- public/atari/demos/defenceforce/TimeSlices/build.bat
+++ public/atari/demos/defenceforce/TimeSlices/build.bat
@@ -2,13 +2,11 @@
 :: http://sun.hasenbraten.de/vasm/index.php?view=tutorial
 :: http://sun.hasenbraten.de/vasm/release/vasm.html
 ::
-del monodemo.prg
-::vasmm68k_mot_win32.exe -Ftos -monst -x -o monodemo.prg monodemo.s
-vasm.exe -m68000 -Ftos -noesc -no-opt -o monodemo.prg monodemo.s
+del temporary.prg
+vasm.exe -m68000 -Ftos -noesc -no-opt -o temporary.prg timslces.s
 
-del timslces.prg
-upx-3.03 --ultra-brute monodemo.prg -o timslces.prg
-copy timslces.prg C:\svn\public\atari\demos\defenceforce\TimeSlices\Final\fixed.prg
-
+del Final\timslces.prg
+upx-3.03 --ultra-brute temporary.prg -o Final\timslces.prg
+del temporary.prg
 
 pause
