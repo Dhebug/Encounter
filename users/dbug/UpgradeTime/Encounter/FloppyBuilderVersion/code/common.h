59516e5bce54ead2f86efe147e120e66d3a08304
dbug <dbug@5b018df4-7644-4ca1-8539-644edd896026>
2023-03-17 20:19:40 +0000

    Added the display of the high scores table (similar to the original BASIC code) Also fixed the map file viewer (was using the wrong file names)

--- users/dbug/UpgradeTime/Encounter/FloppyBuilderVersion/code/common.h
+++ users/dbug/UpgradeTime/Encounter/FloppyBuilderVersion/code/common.h
@@ -20,13 +20,6 @@ extern unsigned char KeyBank[8]; // .dsb 8   ; The virtual Key Matrix
 // Display
 extern unsigned char ImageBuffer[40*200];
 
-extern char Text_CopyrightSevernSoftware[];
-extern char Text_CopyrightDefenceForce[];
-extern char Text_FirstLine[];
-extern char Text_HowToPlay[];
-extern char Text_MovementVerbs[];
-extern char Text_Notes[];
-
 
 // Audio
 extern void PlaySound(const char* registerList);
@@ -51,4 +44,41 @@ extern void WaitFrames(int frames);
 extern char gIsHires;
 extern char* gPrintAddress;
 
+/*
+;
+; Data generator for the high scores
+;
+; Each entry occupies 19 bytes:
+;  2 bytes for the score (+32768)
+;  1 byte for the game ending condition
+; 16 bytes for the name (padded with spaces)
+;-------------------------------------------
+; 19 bytes per entry * 24 entries = 456 bytes total
+;
+#define ENTRY(type,score,name) .word (score+32768) : .byt type : .asc name
+*/
+#define SCORE_COUNT  24
 
+enum SCORE_CONDITION
+{
+    e_SCORE_UNNITIALIZED    = 0,
+    e_SCORE_SOLVED_THE_CASE = 1,
+    e_SCORE_MAIMED_BY_DOG   = 2,
+    e_SCORE_SHOT_BY_THUG    = 3,
+    e_SCORE_FELL_INTO_PIT   = 4,
+    e_SCORE_TRIPPED_ALARM   = 5,
+    e_SCORE_RAN_OUT_OF_TIME = 6,
+    e_SCORE_BLOWN_INTO_BITS = 7,
+    e_SCORE_SIMPLY_VANISHED = 8,
+    e_SCORE_GAVE_UP         = 9
+};
+
+typedef struct 
+{
+    int             score;          // The score can actually be negative if the player is doing stupid things on purpose
+    unsigned char   condition;      // The reason why the game ended (victory, abandon, death, ...)
+    unsigned char   name[16];       // The name of the character  
+} score_entry;
+
+extern score_entry gHighScores[SCORE_COUNT];
+extern const char* gScoreConditionsArray[];
--- users/dbug/UpgradeTime/Encounter/FloppyBuilderVersion/code/intro_utils.s
+++ users/dbug/UpgradeTime/Encounter/FloppyBuilderVersion/code/intro_utils.s
@@ -18,3 +18,30 @@ _Text_HowToPlay                  .byt 1,"           How to play",0
 _Text_MovementVerbs              .byt 1,"   MOVEMENT            VERBS",0
 _Text_Notes                      .byt 1,"              NOTES",4,"    SHOOT SIPHON",0    ; That's a bug there that needs fixing, using zero as a terminator means we can't use black ink because of strcpy
 
+_Text_Leaderboard                .byt 16+1,3,"            Leaderboard",0
+
+_Test_Empty                      .byt 0
+
+_Text_SCORE_SOLVED_THE_CASE      .byt 2,"Solved the case",0
+_Text_SCORE_MAIMED_BY_DOG        .byt 5,"Maimed by a dog",0
+_Text_SCORE_SHOT_BY_THUG         .byt 1,"Shot by a thug",0
+_Text_SCORE_FELL_INTO_PIT        .byt 3,"Fell into a pit",0
+_Text_SCORE_TRIPPED_ALARM        .byt 3,"Tripped the alarm",0
+_Text_SCORE_RAN_OUT_OF_TIME      .byt 6,"Ran out of time",0
+_Text_SCORE_BLOWN_INTO_BITS      .byt 1,"Blown into bits",0
+_Text_SCORE_SIMPLY_VANISHED      .byt 7,"Simply Vanished!",0
+_Text_SCORE_GAVE_UP              .byt 5,"Gave up...",0
+
+_gScoreConditionsArray
+  .word _Test_Empty
+  .word _Text_SCORE_SOLVED_THE_CASE
+  .word _Text_SCORE_MAIMED_BY_DOG  
+  .word _Text_SCORE_SHOT_BY_THUG   
+  .word _Text_SCORE_FELL_INTO_PIT  
+  .word _Text_SCORE_TRIPPED_ALARM  
+  .word _Text_SCORE_RAN_OUT_OF_TIME
+  .word _Text_SCORE_BLOWN_INTO_BITS
+  .word _Text_SCORE_SIMPLY_VANISHED
+  .word _Text_SCORE_GAVE_UP        
+
+
