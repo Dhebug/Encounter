959e273024a0d90500610e930b1f45ef24aab8b0
Mickaël Pointier <Dhebug@users.noreply.github.com>
2023-04-17 18:46:49 +0000

    Restructured the code a bit, moved some tables into unused areas of memory, and extracted the items and locations from an enum and into #defines so they can be used from C or Assembler

---
+++ users/dbug/UpgradeTime/Encounter/FloppyBuilderVersion/code/game_enums.h
@@ -0,0 +1,109 @@
+
+
+// Define the various locations
+#define	e_LOCATION_MARKETPLACE       0
+#define	e_LOCATION_DARKALLEY         1
+#define	e_LOCATION_ROAD              2
+#define	e_LOCATION_DARKTUNNEL        3
+#define	e_LOCATION_MAINSTREET        4
+#define	e_LOCATION_NARROWPATH        5
+#define	e_LOCATION_INSIDEHOLE        6
+#define	e_LOCATION_WELL              7
+#define	e_LOCATION_WOODEDAVENUE      8
+#define	e_LOCATION_GRAVELDRIVE       9 
+#define	e_LOCATION_TARMACAREA       10
+#define	e_LOCATION_ZENGARDEN        11
+#define	e_LOCATION_LAWN             12
+#define	e_LOCATION_GREENHOUSE       13
+#define	e_LOCATION_TENNISCOURT      14
+#define	e_LOCATION_VEGSGARDEN       15
+#define	e_LOCATION_FISHPND          16
+#define	e_LOCATION_TILEDPATIO       17
+#define	e_LOCATION_APPLE_TREES      18
+#define e_LOCATION_DARKCELLARROOM   19
+#define	e_LOCATION_CELLAR           20
+#define	e_LOCATION_NARROWSTAIRCASE  21
+#define	e_LOCATION_LOUNGE           22
+#define	e_LOCATION_ENTRANCEHALL     23
+#define	e_LOCATION_LIBRARY          24
+#define	e_LOCATION_DININGROOM       25
+#define	e_LOCATION_LARGE_STAIRCASE  26
+#define	e_LOCATION_GAMESROOM        27
+#define	e_LOCATION_SUNLOUNGE        28
+#define	e_LOCATION_KITCHEN          29
+#define	e_LOCATION_NARROWPASSAGE    30 
+#define	e_LOCATION_GUESTBEDROOM     31
+#define	e_LOCATION_CHILDBEDROOM     32
+#define	e_LOCATION_MASTERBEDROOM    33
+#define	e_LOCATION_SHOWERROOM       34
+#define	e_LOCATION_TINY_WC          35
+#define	e_LOCATION_EASTGALLERY      36
+#define	e_LOCATION_BOXROOM          37
+#define	e_LOCATION_PADLOCKED_ROOM   38
+#define	e_LOCATION_CLASSY_BATHROOM  39
+#define	e_LOCATION_WESTGALLERY      40
+#define	e_LOCATION_UP_STAIRS        41
+#define	e_LOCATION_OUTSIDE_PIT      42
+#define	e_LOCATION_GIRLROOM         43
+#define e_LOCATION_COUNT_           44
+#define e_LOCATION_INVENTORY        e_LOCATION_COUNT_    // Special location for the player's inventory
+#define e_LOCATION_NONE             255                   // To indicate we can't go in this particular location
+
+
+// Define the various items
+// Define the various items
+// Containers first
+#define	e_ITEM_TobaccoTin    		 0          // an empty tobacco tin
+#define	e_ITEM_Bucket        		 1          // a wooden bucket
+#define	e_ITEM_CardboardBox  		 2          // a cardboard box
+#define	e_ITEM_FishingNet    		 3          // a fishing net
+#define	e_ITEM_PlasticBag    		 4          // a plastic bag
+#define	e_ITEM_SmallBottle  		 5          // a small bottle
+#define	e_ITEM__Last_Container       5          // ----- END CONTAINERS MARKER
+
+// Items requiring containers
+#define	e_ITEM_BlackDust  			 6          // black dust
+#define	e_ITEM_YellowPowder  		 7          // gritty yellow powder
+#define	e_ITEM_Petrol  				 8          // some petrol
+#define	e_ITEM_Water  				 9          // some water
+
+// Then normal items
+#define	e_ITEM_LockedPanel  		 10         // a locked panel on the wall
+#define	e_ITEM_OpenPanel  			 11         // an open panel on wall
+#define	e_ITEM_SmallHoleInDoor 		 12         // a small hole in the door
+#define	e_ITEM_BrokenWindow  		 13          // the window is broken
+#define	e_ITEM_LargeDove  			 14         // a large dove
+#define	e_ITEM_Twine  				 15         // some twine
+#define	e_ITEM_SilverKnife  		 16         // a silver knife
+#define	e_ITEM_Ladder  				 17         // a ladder
+#define	e_ITEM_AbandonedCar  		 18         // an abandoned car
+#define	e_ITEM_AlsatianDog  		 19         // Alsatian dog
+#define	e_ITEM_Meat  				 20         // a joint of meat
+#define	e_ITEM_Bread  				 21         // some brown bread
+#define	e_ITEM_RollOfTape  			 22         // a roll of sticky tape
+#define	e_ITEM_ChemistryBook  		 23         // a chemistry book
+#define	e_ITEM_BoxOfMatches  		 24         // a box of matches
+#define	e_ITEM_SnookerCue  			 25         // a snooker cue
+#define	e_ITEM_Thug  				 26         // a Thug
+#define	e_ITEM_HeavySafe  			 27         // a heavy safe
+#define	e_ITEM_PrintedNote  		 28         // a printed note
+#define	e_ITEM_Rope  				 29         // a length of rope
+#define	e_ITEM_RopeHangingFromWindow 30         // a rope hangs from the window
+#define	e_ITEM_RollOfToiletPaper  	 31         // a roll of toilet tissue~
+#define	e_ITEM_HosePipe  			 32         // a hose-pipe
+#define	e_ITEM_OpenSafe  			 33          // an open safe
+#define	e_ITEM_BrokenGlass  		 34         // broken glass
+#define	e_ITEM_AcidBurn  			 35         // an acid burn
+#define	e_ITEM_YoungGirl  			 36         // a young girl
+#define	e_ITEM_Fuse  				 37         // a fuse
+#define	e_ITEM_GunPowder  			 38         // some gunpowder
+#define	e_ITEM_Keys  				 39         // a set of keys
+#define	e_ITEM_Newspaper     		 40         // A newspaper
+#define	e_ITEM_Bomb  				 41         // a bomb
+#define	e_ITEM_Pistol 				 42         // a pistol
+#define	e_ITEM_Bullets  			 43         // three .38 bullets
+#define	e_ITEM_YoungGirlOnFloor  	 44         // a young girl tied up on the floor
+#define	e_ITEM_COUNT_ 				 45         //  ----- END MARKER
+
+#define	e_ITEM__Reserved      		 45          // Reserved entry code (not sure it's needed)
+// End marker
