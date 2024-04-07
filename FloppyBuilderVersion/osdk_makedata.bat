dafebfb865686ff437fb808c387b26af626b6760
Dbug <mike@defence-force.org>
2024-04-07 15:00:00 +0200

    Added a French version of the "The End" image.

--- FloppyBuilderVersion/osdk_makedata.bat
+++ FloppyBuilderVersion/osdk_makedata.bat
@@ -70,6 +70,7 @@ SET PARAMS=-u1 -m0 -f0 -o4_ArrowBlockMasks
 :: Masked out sprites
 SET PARAMS=-u1 -m0 -f3 -o2
 %PICTCONV% %PARAMS% data\masked_the_end.png %TARGET%\the_end.msk
+%PICTCONV% %PARAMS% data\masked_the_end_fr.png %TARGET%\the_end_fr.msk
 %PICTCONV% %PARAMS% data\masked_dog.png %TARGET%\dog.msk
 %PICTCONV% %PARAMS% data\masked_items.png %TARGET%\items.msk
 %PICTCONV% %PARAMS% data\masked_thug.png %TARGET%\thug.msk
