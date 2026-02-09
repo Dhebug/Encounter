234a6dd5163dd4cb47040d3d5e0c2cb0e6c0fd57
Dbug <mike@defence-force.org>
2026-02-09 12:14:58 +0100

    Removed an unused enum value

--- code/game_enums.h
+++ code/game_enums.h
@@ -196,7 +196,7 @@
 // Flags for the items
 #define ITEM_FLAG_DEFAULT 			0    // Nothing special
 #define ITEM_FLAG_IS_CONTAINER 		1    // This item is a container
-#define ITEM_FLAG_NEEDS_CONTAINER 	2    // This item needs to be transported in a container
+#define ITEM_FLAG_free_to_use    	2    // - available -
 #define ITEM_FLAG_IMMOVABLE			4    // Impossible to move for various reasons
 #define ITEM_FLAG_LOCKED            8    // For item which are locked
 #define ITEM_FLAG_DISABLED         16    // Used to indicate that something is not active anymore (ex: Dog, Thug )
